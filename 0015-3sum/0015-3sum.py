47e73045fefa616f64294370835dbe4f214184e2
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 15:34:35 -0400

    Time: 638 ms (56.35%), Space: 22.3 MB (53.49%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,38 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # create a list to store different triplets
         res = []
+        # sort nums to make it easier to ensure no duplicates
         sort = sorted(nums)
-        
+
+        # find the correct starting point (index i)
         for i in range(len(sort)):
-            # Fix 1: Check against the previous element
+            # index is positive and value is a duplicate then continue to prevent duplicate answers
             if i > 0 and sort[i] == sort[i - 1]:
-                continue
+                continue 
             
+            # set indices for left and right pointer
             p1 = i + 1
             p2 = len(sort) - 1
 
+            # two sum approach
             while p1 < p2:
                 total = sort[i] + sort[p1] + sort[p2]
-                
+
                 if total < 0:
                     p1 += 1
                 elif total > 0:
                     p2 -= 1
+                # append onto result if total is 0
+                # iterate p1/p2 to continue
                 else:
-                    # Fix 2: Typo fixed and brackets added
                     res.append([sort[i], sort[p1], sort[p2]])
-                    
-                    # Fix 3: Indented these so they only trigger on a match
                     p1 += 1
                     p2 -= 1
-
+                
+                # ensures that left pointer is not a duplicate
                     while p1 < p2 and sort[p1] == sort[p1 - 1]:
                         p1 += 1
-                        
         return res
+
+        
