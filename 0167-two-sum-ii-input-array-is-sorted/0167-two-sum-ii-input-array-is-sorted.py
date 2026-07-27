f16bf855646274cc73b13ae96327b25fb5069fd8
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 14:00:54 -0400

    Time: 0 ms (100%), Space: 20.4 MB (75.43%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,12 +4,17 @@ class Solution:
         p1 = 0
         p2 = len(numbers) - 1
 
+        # ensure that pointers don't cross
+        # can't have two same values either
         while p1 < p2:
+            # add the values at curr pointers
             total = numbers[p1] + numbers[p2]
+            # move left pointer if total < target
             if total < target:
                 p1 += 1
+            # move right pointer if total > target
             elif total > target:
                 p2 -= 1 
+            # return indices with +1 because it is using 1-indexed output 
             else:
                 return [p1 + 1, p2 + 1]
-
