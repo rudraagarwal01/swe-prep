f29ba7c7a4a002a5b07084a5262c3db53d532d4b
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 12:55:32 -0400

    Time: 4 ms (42.09%), Space: 20.2 MB (64.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,27 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
+# class Solution:
+#     def twoSum(self, nums, target):
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+# O(N^2)
 
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {} # dictionary 
+        
+        # enumerate iterates and basically stores index and number
+        # Key is the number(complement)
+        # Value is the index
         for i, num in enumerate(nums):
             complement = target - num
-        
+            # Search in seen with complement
+            # if value exists return its index and curr index
             if complement in seen:
                 return [seen[complement], i]
-            else:
+            # if complement not in seen then store curr num in seen for future
+            else: 
                 seen[num] = i
+# O(N)
+
