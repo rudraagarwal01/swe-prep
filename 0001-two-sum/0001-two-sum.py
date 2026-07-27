d0c978802b67e961d331a98c4b29f8d1a857f61d
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 12:53:48 -0400

    Time: 0 ms (100%), Space: 20.4 MB (40.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,11 @@
-# class Solution:
-#     def twoSum(self, nums, target):
-#         n = len(nums)
-#         for i in range(n):
-#             for j in range(i + 1, n):
-#                 if nums[i] + nums[j] == target:
-#                     return [i, j]
-# O(N^2)
-
 class Solution:
-    def twoSum(self, nums, target):
-        seen = {} # dictionary 
-        
-        # enumerate iterates and basically stores index and number
-        # Key is the number
-        # Value is the index
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
         for i, num in enumerate(nums):
             complement = target - num
-            # Search in the hashmap with the key so the number
+        
             if complement in seen:
                 return [seen[complement], i]
-            else: 
+            else:
                 seen[num] = i
-# O(N)
-
