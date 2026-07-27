120ccb714bd0187bb55089672b049a48a6e82086
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 12:01:51 -0400

    Time: 0 ms (100%), Space: 19.3 MB (65.25%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,16 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        stack2 = []
+
+        for char in s:
+            if char != "#": 
+                stack1.append(char)
+            elif stack1: # character is "#" and stack is not empty 
+                stack1.pop()
+        for char in t:
+            if char != "#":
+                stack2.append(char)
+            elif stack2:
+                stack2.pop()
+        return stack1 == stack2
