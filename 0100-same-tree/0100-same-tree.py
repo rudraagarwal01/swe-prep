305b55c40ae782151945e248a11efbf867e474f2
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 12:05:36 -0400

    Time: 0 ms (100%), Space: 19.3 MB (71.89%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # use deque for Trees
+        
+        # create deque with both p and q (tuple)
+        queue = deque([(p, q)]) 
+
+        while queue:
+            # pop the first node (root) in each tree
+            node1, node2 = queue.popleft()
+
+            # if both empty then continue 
+            if not node1 and not node2:
+                continue 
+            
+            # if either one is empty or they are not equal then return false
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            
+            # add their children to conintue comparison
+            # compare as tuples!!
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+    
+        return True
+
+            
+            
+
+
+    
