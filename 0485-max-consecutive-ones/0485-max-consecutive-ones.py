bff58d1e33bb85b2b94716c2b04654a4656fe491
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 16:34:27 -0400

    Time: 7 ms (96.13%), Space: 22.1 MB (5.04%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,14 +3,17 @@ class Solution:
         max = 0
         temp = 0
 
+        # iterate through nums
+        # add to temp if num is 1
         for num in nums:
             if num == 1:
                 temp += 1
+            # if temp is greater than max then update max
             else:
                 if temp > max:
                     max = temp
                 temp = 0
-
+        # update max
         if temp > max:
             max = temp
             
