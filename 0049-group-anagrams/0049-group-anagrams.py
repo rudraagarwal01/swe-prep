3a262228865c2d2296b260099e151b05a0d74fa8
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-27 13:30:13 -0400

    Time: 13 ms (49.51%), Space: 21.7 MB (97.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # create dict to store different groups of letters
         seen = {}
 
+        # use sorted to store the keys and ensure that the words are grouping with the right key
         for word in strs:
             key = "".join(sorted(word))
 
+            # if the key is in seen then append it to curr key
             if key in seen:
                 seen[key].append(word)
+            # else create a new key and add the curr word
             else:
                 seen[key] = [word]
+                
+        # return the dict as a list
         return list(seen.values())
