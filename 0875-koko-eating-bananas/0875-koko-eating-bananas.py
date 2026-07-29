ae57cfbf07b4c751df1267b503bc5d6737c79152
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-28 22:13:56 -0400

    Time: 175 ms (40.9%), Space: 20.8 MB (11.51%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,26 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # least hours to finish one pile
+        # lowest amount of time to eat a pile is one hour
         left = 1
-        # find the time it takes her to eat the pile with the most bananas
-        # so if hours are 11 it would take 11 bananas/hr highest time
+        # maximum amount of time to eat the largest pile of bananas
         right = max(piles)
 
-        while left < right: 
-            # (1 + 11) // = 6
+        while left < right:
+            # find the time in the middle to see if that would work
             mid = (left + right) // 2
-            
-            # calculate the total hours it would take to eat at the mid rate
+
+            # calculate the total hours it takes to eat the bananas at middle rate
             total_hours = 0
             for p in piles:
                 total_hours += math.ceil(p / mid)
-            
+
+            # if total hours is > given h then we need to increase the speed to greater than the middle
             if total_hours > h:
                 left = mid + 1
-            # if mid speed is correct then try to find speed lower 
-            # doing right = mid - 1 could force it to find a speed lower, when mid is the right speed
+            # if total hours is <= given h then that might be correct speed or too slow
+            # continue to check the slower rate by updating the right index
             else:
                 right = mid
         return left
-
-            
