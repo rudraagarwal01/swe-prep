642ebf4051899c5fa7c6198211927c23ea80feb2
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-07-28 21:26:30 -0400

    Time: 0 ms (100%), Space: 19.6 MB (24.34%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -15,12 +15,19 @@ class Solution:
         left = 0
         right = len(nums) - 1
 
+        # Use two pointer approach
+        # find the mid index
         while left < right:
             mid = (left + right) // 2
 
+            # compare the num at mid index and the last number in array
+            # if mid num is bigger than right num then move the left index to mid + 1
+            # we know that all nums to the left of mid can be elimated
             if nums[mid] > nums[right]:
                 left = mid + 1
+            # if mid num is smaller we know that everything to the right of mid can be elimated
             else:
                 right = mid
         return nums[left]
-        
+
+# O(log n)
