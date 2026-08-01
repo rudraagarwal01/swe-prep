e512a67097529d096af2346be39c38f883ec1563
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-08-01 16:40:10 -0400

    Time: 19 ms (16.45%), Space: 20.3 MB (18.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # sort both strings and compare them
         if sorted(s) == sorted(t): 
             return True
         return False
+# O(n log n)
+
+
+
 
 
