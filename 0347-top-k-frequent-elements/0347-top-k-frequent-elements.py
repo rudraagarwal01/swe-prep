c1dd177e1426278a5070370dd7708df0bf184904
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-08-04 14:23:39 -0400

    Time: 5 ms (62.17%), Space: 22.9 MB (41.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,32 +1,3 @@
-# class Solution:
-#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-#         # return the elements that show up the most (return k elements)
-
-#         # create result array
-#         res = []
-
-#         count = Counter(nums)
-
-#         # create an array of empty lists where index represents frequency 
-#         # The max possible frequency is len(nums), so we need an array of size len(nums) + 1
-#         freq = []
-#         for _ in range(len(nums) + 1):
-#             freq.append([])
-        
-#         # Group numbers by their frequency
-#         # 'c' is the frequency, so we put the 'num' into the list at index 'c'
-#         for num, c in count.items():
-#             freq[c].append(num)
-
-#         # iterate from the highest possible frequency down to 1
-#         for i in range(len(freq) - 1, 0, -1):
-#             for num in freq[i]:
-#                 res.append(num)
-#                 # Once we have k elements, we are done
-#                 if len(res) == k:
-#                     return res
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # can sort the array first and use two pointer to add amount of same element
@@ -55,4 +26,32 @@ class Solution:
             # add the last elements
             res.append(stack.pop()[1])
         return res
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         # return the elements that show up the most (return k elements)
+
+#         # create result array
+#         res = []
+
+#         count = Counter(nums)
+
+#         # create an array of empty lists where index represents frequency 
+#         # The max possible frequency is len(nums), so we need an array of size len(nums) + 1
+#         freq = []
+#         for _ in range(len(nums) + 1):
+#             freq.append([])
+        
+#         # Group numbers by their frequency
+#         # 'c' is the frequency, so we put the 'num' into the list at index 'c'
+#         for num, c in count.items():
+#             freq[c].append(num)
+
+#         # iterate from the highest possible frequency down to 1
+#         for i in range(len(freq) - 1, 0, -1):
+#             for num in freq[i]:
+#                 res.append(num)
+#                 # Once we have k elements, we are done
+#                 if len(res) == k:
+#                     return res
         
