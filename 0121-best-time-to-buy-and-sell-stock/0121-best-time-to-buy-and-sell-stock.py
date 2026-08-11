e91a99f54d009a368e367210268346bec3257618
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-08-11 17:39:24 -0400

    Time: 87 ms (5.46%), Space: 28.7 MB (40.84%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Cannot do sort because the indices are basically days 
+        # and we cannot sell on a day before it is bought
+
+        # Use sliding window to find the difference each time
+
+        profit = 0
+        max_profit = 0
+        left = 0
+        right = 1
+
+        while right < len(prices):
+            # if profit
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                max_profit = max(max_profit, profit)
+            else:
+                # Cheaper buying point (basically if loss was calculated)
+                # if right finds a lower price then move the left pointer there
+                left = right
+            right += 1
+
+        return max_profit
+
+
+            
+
+
