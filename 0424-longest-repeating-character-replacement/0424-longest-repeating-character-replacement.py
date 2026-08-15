99f753494db5aa4dba109b19a6f3901c44ad9ff1
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-08-14 20:44:59 -0400

    Time: 83 ms (59.56%), Space: 19.4 MB (97.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,5 +1,7 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # dict to store the freq
+        # char -> freq
         count = {}
         left = 0
         max_len = 0
@@ -7,12 +9,20 @@ class Solution:
 
         for right in range(len(s)):
             # 1. Expand the window by including s[right]
-            count[s[right]] = count.get(s[right], 0) + 1
+            # This basically gets the letter of the most frequency
+            char = s[right]
+
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
             max_freq = max(max_freq, count[s[right]])
 
             # 2. If replacements needed exceed k, shrink the window from the left
             while (right - left + 1) - max_freq > k:
+                # remove from count
                 count[s[left]] -= 1
+                # shrink window
                 left += 1
 
             # 3. Update the maximum valid window length
