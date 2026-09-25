9e99ade054dd75536e71dc17b6bfcead51d16fbb
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-25 15:41:55 -0400

    Time: 0 ms (100%), Space: 19.2 MB (87.12%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -5,9 +5,6 @@ class Solution:
 
         for num in nums:
             curr += num
-            # stores the all time lowest number (usually negative)
             min_prefix = min(min_prefix, curr)
 
-        # return 1 if all positives in nums
-        # return 1 - negative number to ensure you get prefix > 1
         return max(1, 1 - min_prefix)
