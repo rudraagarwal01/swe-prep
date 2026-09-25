d7f6ab2956db8970e2be6a1d7255d6955de1d9fb
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-25 17:00:30 -0400

    Time: 7 ms (85.28%), Space: 37.8 MB (57.53%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,6 +1,7 @@
 class Solution:
     def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
-        i, j = 0, 0
+        i = 0
+        j = 0
 
         while i < len(nums1) and j < len(nums2):
             if nums1[i] == nums2[j]:
@@ -9,5 +10,5 @@ class Solution:
                 i += 1
             else:
                 j += 1
-
-        return -1  # no common element found
+        
+        return -1
