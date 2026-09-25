25aeadb377bb05b81c620d0fee0613d2108b2eb0
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-25 13:50:33 -0400

    Time: 46 ms (57.49%), Space: 24.5 MB (24.28%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,11 +6,13 @@ class Solution:
             if nums[right] == 0:
                 curr += 1
 
+            # removes all zeros from left except 1 (if there)
             while curr > 1:
                 if nums[left] == 0:
                     curr -= 1
                 left += 1
             
+            # not right - left + 1 because you always need to remove 1
             ans = max(ans, right - left)
-            
+
         return ans
