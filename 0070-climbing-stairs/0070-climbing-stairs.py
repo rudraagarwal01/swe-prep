0948dae121fdf5884a1a0b5c65333594c0d0e75c
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-26 16:49:42 -0400

    Time: 0 ms (100%), Space: 19.2 MB (87.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,11 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2:
             return n
-        
+
+        # dp[i] = number of distinct ways to reach step i
         dp = [0] * (n + 1)
         dp[1] = 1
         dp[2] = 2
 
         for i in range(3, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
+
         return dp[n]
