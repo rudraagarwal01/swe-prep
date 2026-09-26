dd4e161bd680e02db637822acfce5e90920eb74d
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-26 13:36:00 -0400

    Time: 6 ms (7.03%), Space: 19.1 MB (99.54%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -8,5 +8,7 @@ class Solution:
             counts[c] += 1
 
         freq = counts.values()
+        # if all freq are the same then it will be one element in set
+        # if there are freq or 1, 2 then two elements and this condition will fail
         return len(set(freq)) == 1  
 
