700fbfd3fee939f1fa40bf7ff09a7868e2495daa
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-26 13:16:29 -0400

    Time: 3 ms (56.92%), Space: 19.5 MB (71.8%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -1,19 +1,18 @@
 from collections import defaultdict
-
 class Solution:
     def intersection(self, nums: list[list[int]]) -> list[int]:
         counts = defaultdict(int)
 
         for arr in nums:
             for i in arr:
-                # add frequency of each number
                 counts[i] += 1
         
-        n = len(nums)
+
         ans = []
+        n = len(nums)
+
         for key in counts:
             if counts[key] == n:
                 ans.append(key)
-        
-        return sorted(ans)
 
+        return sorted(ans)
