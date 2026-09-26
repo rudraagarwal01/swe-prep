a3c3066aa68133562144eaa28689a6965c989bc9
Rudra Agarwal <139291673+rudraagarwal01@users.noreply.github.com>
2026-09-26 11:54:02 -0400

    Time: 0 ms (100%), Space: 20.5 MB (42.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -9,14 +9,19 @@
 
 class Solution:
     def twoSum(self, nums, target):
-        seen = {}
-
+        seen = {} # dictionary 
+        
+        # enumerate iterates and basically stores the index and number
+        # Key is the number(complement)
+        # Value is the index
         for i, num in enumerate(nums):
             complement = target - num
-
+            # Search in seen with complement
+            # if value exists return its index and curr index
             if complement in seen:
                 return [seen[complement], i]
-            else:
+            # if complement not in seen then store curr num in seen for future
+            else: 
                 seen[num] = i
 # O(N)
 
